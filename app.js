8ed122174b6473fd6a6b990cd450df607ff00c64
rithaven <veneruth4@gmail.com>
2021-11-23 20:01:16 +0200

    Create TAJI project

---
+++ app.js
@@ -0,0 +1,34 @@
+ function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const menu=document.querySelector('#mobile-menu');
+// const menuLinks=document.querySelector('.navbar__menu');
+
+// menu.addEventListener('click', function() {
+//    menu.cl.toggle('is-active');
+//    menuLinks.classList.toggle('active');
+// });
